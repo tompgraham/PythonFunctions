3693ad2afa40e7bf3c291caf928b890e104b6d07
tompgraham <tgraha03@uw.edu>
2016-10-05 19:10:49 -0700

    InitialCommit
    SineFunction

---
+++ Sine.py
@@ -0,0 +1,66 @@
+#===============================================================================
+#                               General Documentation
+"""Single Function Module
+
+    See function docstring for description.
+"""
+
+#-------------------------------------------------------------------------------
+#                               Addition Documentation
+#
+#
+#Modification History
+# - Created 10/5/2016 by Tom Graham for CSS 455, University of Washington, Bothell
+#
+#Notes:
+# - Written for Python 2.7.12
+#===============================================================================
+
+
+def sine(value, tol):
+    """Calculate the sine of a decimal number
+    
+    Method Arguments:
+    * value:  The value this function will calculate the sine of.  The value
+      is a numeric floating point number.
+    
+    * Tol: The level of precision desired for the sine calculation.  Is a
+      percentage of the end result.  I.e. if passed .0001, will calculate the
+      answer to a .01% tolerance as the end result converages.  If passed None
+      will default to a .0001% tolerance.
+    
+    Output: 
+    * Sine of the value.  Numeric floating point number called 'sum'
+      representing the numeric end result of the sine of the initial value. 
+    
+    The sine is calculated via a series expansion.  Given we can't extend the
+    series infinitely, the cut off is determined by the tolerance variable.
+    
+    """
+    
+    #- Import Statements:
+    
+    import math
+    
+    #- Initialize variables
+    
+    n = 1
+    sumPrev = 0.0
+    sum = value
+    
+    #- If no tolerance specified, initializes the tol variable
+    
+    if (tol == None):
+        tol = .000001
+    
+    #- Calculate the sin of the value using a series expansion
+    
+    while(abs((sum-sumPrev)/sum) > tol):
+        sumPrev = sum
+        sum = sum + ((-1)**n)*(x**(2 * n + 1)/math.factorial(2 * n + 1))   
+        n = n + 1
+
+    #- Returns sine of the value
+
+    return sum
+
